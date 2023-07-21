30be2daed320f7972d98367495b1494f6caed238
XXY126 <francescoxustudente@gmail.com>
2023-07-21 02:41:00 +0200

    +AddOrdineServlet

--- src/main/java/controller/AddOrdineServlet.java
+++ src/main/java/controller/AddOrdineServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.Date;
@@ -8,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +25,7 @@ import javax.servlet.http.HttpSession;
 import model.Carrello;
 import model.Prodotto;
 import model.Utente;
+import model.Indirizzo;
 
 @WebServlet("/AddOrdineServlet")
 public class AddOrdineServlet extends HttpServlet {
@@ -35,78 +38,133 @@ public class AddOrdineServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		Statement s = null;
-		PreparedStatement ps = null;
-		
 		RequestDispatcher dispatcher = null;
 		Utente user = (Utente) session.getAttribute("user");
 		Carrello carrello = (Carrello) session.getAttribute("carrello");
+		PrintWriter out = response.getWriter();
+		String totale = request.getParameter("totale");
 		
 		int address_id = -1;
 
 		try (Connection connection = DbManager.getConnection();){		
-			String query = "SELECT * FROM indirizzo WHERE id_utente = " + user.getEmail();
-			s = connection.createStatement();
-			ResultSet rs = s.executeQuery(query);
-
-			if (rs.next())
-				address_id = rs.getInt("id_indirizzo");
-			else {
-				request.setAttribute("status", "noAddress");
-				dispatcher = request.getRequestDispatcher("indirizzo.jsp");
-				dispatcher.forward(request, response);
-				return;
-			}
-
-			Calendar c = Calendar.getInstance();
-			java.util.Date javaDate = c.getTime();
-			java.sql.Date sqlDate = new Date(javaDate.getTime());
-
-			double totale = Double.parseDouble(request.getParameter("totale"));
-			SecureRandom random = new SecureRandom();
-			int ordine_id = 10000 + random.nextInt(90000);
-
-			query = "INSERT INTO ordine (id, data, totale, site_user_id, stato_ordine_id, metodo_spedizione_id, address_id) values(?, ?, ?, ?, ?, ?, ?)";
-			ps = connection.prepareStatement(query);
-			ps.setInt(1, ordine_id);
-			ps.setDate(2, sqlDate);
-			ps.setDouble(3, totale);
-			ps.setString(4, user.getEmail());
-			ps.setInt(5, 1); // completato
-			ps.setInt(6, 1); // posteItaliane
-			ps.setInt(7, address_id);
-			ps.executeUpdate();
-
+			String query = "";
+			
 			String[] values = request.getParameter("quantita").split(",");
 			int i = 0;
-
 			for (Prodotto p : carrello.getCarrello()) {
+				System.out.println("quantita[" +i+"] = "+values[i]);
 				p.setQuantita(Integer.parseInt(values[i]));
 				i++;
-			}
-			
-			for (Prodotto p : carrello.getCarrello()) {
-				query = "INSERT INTO ordine_singolo (quantità, totale_parziale, ordini_id, prodotti_isbn, prodotti_nome, prodotti_img) values("
-						+ p.getQuantita() + ", " + (p.getPrezzo() * p.getQuantita()) + ", " + ordine_id + ", '"
-						+ p.getIdProdotto() + "', '" + p.getNome() + "', '" + p.getImg() + "')";
 				
-				s.executeUpdate(query);
-				query= "SELECT quantita FROM prodotti WHERE isbn = ?";
-				ps = connection.prepareStatement(query);
-				ps.setString(1,p.getIdProdotto());
-				rs = ps.executeQuery();
+				query = "SELECT * FROM prodotto WHERE IDProdotto = ?";
+				PreparedStatement ps = connection.prepareStatement(query);
+				ps.setString(1, p.getIdProdotto());
+				ResultSet rs = ps.executeQuery();
+				System.out.println("debug AddOrdine:" +query+ " eseguita");
 				if(rs.next()) {
-					query= "UPDATE prodotti SET quantita = ? WHERE isbn = ?";
-					ps = connection.prepareStatement(query);
-					ps.setInt(1, (rs.getInt("quantita")-p.getQuantita()));
-					ps.setString(2, p.getIdProdotto());
-					ps.executeUpdate();
-					ps.close();
+					System.out.println("p.getQuantita = "+p.getQuantita());
+					System.out.println("rs.getIntQuantita = " + rs.getInt("quantita"));
+					if(p.getQuantita()>rs.getInt("quantita")) {
+						out.print("errore quantita");
+						dispatcher = request.getRequestDispatcher("profilo.jsp");
+						dispatcher.forward(request, response);
+					}
+				}else {
+					//errore
 				}
 			}
+			query="SELECT * FROM indirizzo where id_utente = ?";
+			PreparedStatement ps2 = connection.prepareStatement(query);
+			ps2.setString(1, user.getEmail());
+			String id_indirizzo = null;
+			ResultSet rs2 = ps2.executeQuery();
+			if(rs2.next()) {
+				System.out.println("debug AddOrdine: indirizzo");
+				id_indirizzo = rs2.getString("id_indirizzo");
+			}else {
+				System.out.println("debug AddOrdine: errore indirizzo");
+				out.print("errore indirizzo");
+				dispatcher = request.getRequestDispatcher("profilo.jsp");
+				dispatcher.forward(request, response);
+			}
+			
+			
+			
+			//INSERT DELL'ORDINE
+			query = "INSERT INTO ordine (totale, varData, id_utente, id_indirizzo) VALUES(?, ?, ?, ?)";
+			
+			PreparedStatement ps1 = connection.prepareStatement(query);
+			ps1.setString(1, totale);
+			ps1.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+			ps1.setString(3, user.getEmail());
+			ps1.setString(4, id_indirizzo);
+			
+			int rs1 = ps1.executeUpdate();
+			
+			if (rs1==0) {
+				out.print("errore inserimento");
+				dispatcher = request.getRequestDispatcher("profilo.jsp");
+				dispatcher.forward(request, response);
+			}
+			
+			//ELIMINARE QUANTITA DAL DB
+			for(Prodotto p : carrello.getCarrello()) {
+				query = "SELECT * FROM prodotto WHERE IDProdotto = ?";
+				PreparedStatement pp = connection.prepareStatement(query);
+				pp.setString(1, p.getIdProdotto());
+				ResultSet rp = pp.executeQuery();
+				System.out.println("debug: "+query+" eseguita");
+				int quantita=0;
+				if(rp.next()) {
+					quantita = rp.getInt("quantita");
+				}
+				query = "UPDATE prodotto SET quantita = ? WHERE IDProdotto = ?";
+				System.out.println("quantita = "+quantita);
+				PreparedStatement pp1 = connection.prepareStatement(query);
+				pp1.setInt(1, quantita-p.getQuantita());
+				pp1.setString(2, p.getIdProdotto());
+				int rp1 = pp1.executeUpdate();
+				
+				if(rp1 == 0) {
+					System.out.println("errore update");
+					out.print("errore update");
+					dispatcher = request.getRequestDispatcher("profilo.jsp");
+					dispatcher.forward(request, response);
+				}
+				System.out.println("debug: "+query+" eseguita");
+			}
+			
+			//INSERT TABELLA POPOLA
+			query = "SELECT * FROM ordine WHERE id_utente = ? ORDER BY varData DESC";
+			PreparedStatement ps3 = connection.prepareStatement(query);
+			ps3.setString(1, user.getEmail());
+			ResultSet rs3 = ps3.executeQuery();
+			if(rs3.next()) {
+				String id_ordine = rs3.getString("id_ordine");
+				for (Prodotto p : carrello.getCarrello()) {
+					query = "INSERT INTO popola (id_ordine, id_prodotto, quantita, costoAcquisto) values (?, ?, ?, ?)";
+					PreparedStatement ps4 = connection.prepareStatement(query);
+					ps4.setString(1, id_ordine);
+					ps4.setString(2, p.getIdProdotto());
+					ps4.setInt(3, p.getQuantita());
+					ps4.setDouble(4, p.getPrezzo());
+					
+					int rs4 = ps4.executeUpdate();
+					if(rs4==0) {
+						//errore
+					}
+				}
+			}
+			
+			//SVUOTARE IL CARRELLO SIA DALLA SESSIONE CHE NEL DATABASE
+			query="DELETE FROM contiene WHERE id_carrello = ?";
+			PreparedStatement ps5 = connection.prepareStatement(query);
+			ps5.setString(1, carrello.getId());
+			System.out.println(carrello.getId());
+			ps5.executeUpdate();
+			
 			carrello.empty();
 			session.setAttribute("carrello", carrello);
-
 			dispatcher = request.getRequestDispatcher("profilo.jsp");
 			dispatcher.forward(request, response);
 		} catch (SQLException e) {
@@ -117,16 +175,7 @@ public class AddOrdineServlet extends HttpServlet {
 			logger.log(Level.ALL, error, e);
 		} catch (NumberFormatException e) {
 			logger.log(Level.ALL, error, e);
-		} finally {
-			try {
-				s.close();
-				ps.close();
-			} catch (SQLException e) {
-				logger.log(Level.ALL, error, e);
-			} catch (NullPointerException e) {
-				logger.log(Level.ALL, error, e);
-			}
-		}
+		} 
 	}
 
 }
